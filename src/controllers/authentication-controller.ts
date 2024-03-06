426fb0dd2aec7efd2e4dbbdf4dd6183ba9d40643
matheus <matheusg_souza@outlook.com>
2024-03-06 11:29:45 -0300

    feat: Removing serverless feature and changing Github access_token request format

--- src/controllers/authentication-controller.ts
+++ src/controllers/authentication-controller.ts
@@ -4,8 +4,13 @@ export class ExchangeCode {
 	async exchangeCode(code: string) {
 		try {
 			const response = await axios.post(
-				`https://github.com/login/oauth/access_token?client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}&code=${String(code)}`,
-				{},
+				`https://github.com/login/oauth/access_token`,
+				{
+					client_id: process.env.GITHUB_CLIENT_ID,
+					client_secret: process.env.GITHUB_CLIENT_SECRET,
+					code: code,
+					redirect_uri:"exp://192.168.15.147:8081",
+				},
 				{
 					headers: {
 						Accept: "application/json",
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import serverless from "serverless-http";
 import { ExchangeCode } from "@controllers/authentication-controller";
 
 const app = express();
+const port = 8080;
 const exchangeCodeController = new ExchangeCode();
 
 app.use(bodyParser.json());
@@ -20,11 +20,13 @@ app.post("/authenticate", async (req, res) => {
 
 	try {
 		const response = await exchangeCodeController.exchangeCode(code);
-		res.status(200).json({ access_token: response?.data });
+		res.status(200).json(response?.data);
 	} catch (error: unknown) {
 		console.error("Error exchanging code for token:", error);
 		res.status(500).json({ error });
 	}
 });
 
-export const handler = serverless(app);
+app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`);
+});
